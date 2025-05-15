48cf373aa3d79dee0d369f4192e38147bf756357
chung <chung@HOANGKIM>
2025-05-16 00:12:39 +0700

    Add project files.

---
+++ Buoi3/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Buoi3.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Buoi3.Controllers
+{
+    public class StudentController : Controller
+    {
+        // Giả lập danh sách lưu thông tin sinh viên
+        private static List<Student> students = new List<Student>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ShowKQ(Student student)
+        {
+            students.Add(student);
+
+            // Đếm số lượng SV cùng chuyên ngành
+            int count = students.Count(s => s.ChuyenNganh == student.ChuyenNganh);
+
+            ViewBag.MSSV = student.MSSV;
+            ViewBag.HoTen = student.HoTen;
+            ViewBag.ChuyenNganh = student.ChuyenNganh;
+            ViewBag.Count = count;
+
+            return View();
+        }
+    }
+}
